5fda7b592a8ed5de39de93709823613ca0c0021a
Siddhesh <64964929+siddheshsule@users.noreply.github.com>
2022-04-29 21:19:18 +0200

    Array practice programs

---
+++ Java_developer_course/Java_example_excercises/ArrayPrac.java
@@ -0,0 +1,11 @@
+public class ArrayPrac {
+    public static void main(String[] args) {
+
+        int arr1[] = {1,2,3,4,5,6,7,8,9,10,11};
+
+        for(int x:arr1){
+            System.out.println(x * 3);
+        }
+    }
+    
+}
